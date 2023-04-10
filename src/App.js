2b83431705ae6faf5f5f46de945bb93e9e9b7fac
Siddhesh More <msiddhesh008@gmail.com>
2023-04-10 14:14:35 +0530

    dep

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './Components/Navbar';
-import RollDice from "./Components/ProjectsFolder/RollDice/RollDice"
-import Home from './Pages/Home';
-import FlipCoin from "./Components/ProjectsFolder/FlipCoin/FlipCoin"
-import Todo from "./Components/ProjectsFolder/Todo/Todo"
-import Pokedex from "./Components/ProjectsFolder/Pokedex/App"
-
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import RollDice from "./Components/ProjectsFolder/RollDice/RollDice";
+import Home from "./Pages/Home";
+import FlipCoin from "./Components/ProjectsFolder/FlipCoin/FlipCoin";
+import Todo from "./Components/ProjectsFolder/Todo/Todo";
+import Pokedex from "./Components/ProjectsFolder/Pokedex/App";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={ <Home /> } />
-        <Route path='/rolldice' element={ <RollDice /> } />
-        <Route path='/flipcoin' element={ <FlipCoin/> } />
-        <Route path='/todo' element={ <Todo/> } />
-        <Route path='/pokedex' element={ <Pokedex/> } />
-
+        <Route path="/react-portfolio-web-app" element={<Home />} />
+        <Route path="/rolldice" element={<RollDice />} />
+        <Route path="/flipcoin" element={<FlipCoin />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/pokedex" element={<Pokedex />} />
       </Routes>
-        
-      
-      
-
-      
     </div>
   );
 }
